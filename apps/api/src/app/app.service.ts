8f3590f17d173579c7cb57340a806181f57a7ffa
jaybell <jaycooperbell@gmail.com>
2022-11-09 07:22:37 -0800

    ds

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
 
 @Injectable()
 export class AppService {
+  constructor(private prismaService:PrismaService) {
+  }
+
   getData(): { message: string } {
+    this.prismaService;
     return { message: 'Welcome to api!' };
   }
 }
